362cb0bc787d88d04397357b23668d5a9e877a04
candr006 <candr006@ucr.edu>
2018-10-12 20:09:27 +0000

    handle when a=0

--- hw1_10.py
+++ hw1_10.py
@@ -14,49 +14,53 @@ print("")
 print("******* Solutions *********")
 
 #check for complex roots
-sqrtval=(math.pow(b,2)-(4*a*c))
-i_val='';
+try:
+	sqrtval=(math.pow(b,2)-(4*a*c))
+	i_val='';
+except OverflowError, e:
+	print('ERROR: Your number(s) are too large')
+	exit()
 if sqrtval<0:
-	print('here1')
 	i_val='i'
 	sqrtval=-1*(sqrtval)
 
-if b>=0:
-	print('here2')
+if a!=0:
 	num1=((-1*b)-(math.sqrt(sqrtval)))
 	denom1=(2*a)
 	if denom1!=0:
-		print('here3')
 		root1=num1/denom1
 		print('Root 1: '+repr(root1)+i_val)
 	else:
-		print('here4')
-		print("Root 1- ERROR: can't divide by 0")
+		print("Root 1: NONE")
 
-	num2=(2*c)
-	denom2= ((-1*b)+(math.sqrt(sqrtval)))
+	num2=((-1*b)+(math.sqrt(sqrtval)))
+	denom2= (2*a)
 	
 	if denom2!=0:
-		print('here5')
-		root2=num2/denom2
-		print('Root 2: '+repr(root2)+i_val)
-	else:
-		print("Root 2- ERROR: can't divide by 0")
+		try:
+			root2=num2/denom2
+			print('Root 2: '+repr(root2)+i_val)
+		except OverflowError, e:
+			print("Result too large")
+			exit()
 
-if b<0:
-	print('here6')
+	else:
+		print("Root 2: NONE")
+else:
 	num3=(2*c)
 	denom3=((-1*b)-(math.sqrt(sqrtval)))
 	if denom3!=0:
 		root1=num3/denom3
 		print('Root 1: '+repr(root1)+i_val)
+	else:
+		print("Root 1: NONE")
 
-	num4=((-1*b)+(math.sqrt(sqrtval)))
-	denom4=(2*a)
+	num4=(2*c)
+	denom4=((-1*b)+(math.sqrt(sqrtval)))
 	if denom4!=0:
 		root2=num4/denom4
 		print('here7')
 		print('Root 2: '+repr(root2)+i_val)
-	
+
 	else:
-		print("Root 2- ERROR: can't divide by 0")
+		print("Root 2: NONE")
