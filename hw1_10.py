2f122d0d8a9a3f3f830266ece25756455fcbc55f
candr006 <candr006@ucr.edu>
2018-10-13 02:35:56 +0000

    prevent underflow

--- hw1_10.py
+++ hw1_10.py
@@ -5,11 +5,23 @@ print '*************************************************************************
 print "INPUT NOTE: To enter a number of the form '6*10^154', please type '6e154'"
 print '*************************************************************************'
 a=raw_input("Enter value for a: ")
+a_t=a
 a=float(a)
+if a_t!='0' and a==0:
+	print('WARNING: Your number will cause an underflow. Exiting.')
+	exit()
 b=raw_input("Enter value for b: ")
+b_t=b
 b=float(b)
+if b_t!='0' and b==0:
+	print('WARNING: Your number will cause an underflow. Exiting.')
+	exit()
 c=raw_input("Enter value for c: ")
+c_t=c
 c=float(c)
+if b_t!='0' and b==0:
+	print('WARNING: Your number will cause an underflow. Exiting.')
+	exit()
 print("")
 print("******* Solutions *********")
 
@@ -18,7 +30,10 @@ try:
 	sqrtval=(math.pow(b,2)-(4*a*c))
 	i_val='';
 except OverflowError, e:
-	print('ERROR: Your number(s) are too large')
+	print('WARNING: Your number(s) will cause an overflow. Exiting.')
+	exit()
+except ArithmeticError,e:
+	print('WARNING: '+e+'. Exiting.')
 	exit()
 
 #use original formula when a!=o
@@ -33,8 +48,15 @@ if a!=0:
 
 	num1=((-1*b)-(math.sqrt(sqrtval)))
 	if denom1!=0:
-		root1=num1/denom1
-		print('Root 1: '+repr(root1)+i_val)
+		try:
+			root1=num1/denom1
+			print('Root 1: '+repr(root1)+i_val)
+		except OverflowError, e:
+			print('WARNING: Your number(s) will cause an overflow. Exiting.')
+			exit()
+		except ArithmeticError,e:
+			print('WARNING: '+e+'. Exiting.')
+			exit()		
 	else:
 		print("Root 1: NONE")
 
@@ -46,7 +68,10 @@ if a!=0:
 			root2=num2/denom2
 			print('Root 2: '+repr(root2)+i_val)
 		except OverflowError, e:
-			print("Result too large")
+			print('WARNING: Your number(s) will cause an overflow. Exiting.')
+			exit()
+		except ArithmeticError,e:
+			print('WARNING: '+e+'. Exiting.')
 			exit()
 
 	else:
@@ -62,16 +87,30 @@ else:
 		exit()
 
 	if denom3!=0:
-		root1=num3/denom3
-		print('Root 1: '+repr(root1)+i_val)
+		try:
+			root1=num3/denom3
+			print('Root 1: '+repr(root1)+i_val)
+		except OverflowError, e:
+			print('WARNING: Your number(s) will cause an overflow. Exiting.')
+			exit()
+		except ArithmeticError,e:
+			print('WARNING: '+e+'. Exiting.')
+			exit()
 	else:
 		print("Root 1: NONE")
 
 	num4=(2*c)
 	denom4=((-1*b)+(math.sqrt(sqrtval)))
 	if denom4!=0:
-		root2=num4/denom4
-		print('Root 2: '+repr(root2)+i_val)
+		try:
+			root2=num4/denom4
+			print('Root 2: '+repr(root2)+i_val)
+		except OverflowError, e:
+			print('WARNING: Your number(s) will cause an overflow. Exiting.')
+			exit()
+		except ArithmeticError,e:
+			print('WARNING: '+e+'. Exiting.')
+			exit()			
 
 	else:
 		print("Root 2: NONE")
